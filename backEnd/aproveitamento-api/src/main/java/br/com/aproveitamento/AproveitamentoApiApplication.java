fd0e99fbd01d7abbb7451720400b65bece1b1f29
Jackson <balestjackson@gmail.com>
2023-10-06 00:11:02 -0300

    Criação inicial do back end

---
+++ backEnd/aproveitamento-api/src/main/java/br/com/aproveitamento/AproveitamentoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.aproveitamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AproveitamentoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AproveitamentoApiApplication.class, args);
+	}
+
+}
---
+++ backEnd/aproveitamento-api/src/test/java/br/com/aproveitamento/AproveitamentoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aproveitamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AproveitamentoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
