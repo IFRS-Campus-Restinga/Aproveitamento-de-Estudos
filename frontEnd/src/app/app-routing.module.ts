fc691132b273dc2a1ee6e546feacad977b2d9156
GustavoGiglio <gtsgiglio@gmail.com>
2023-10-09 17:35:22 -0300

    feat(discipline-registration): adicionado página de cadastro de disciplinas, favicon e realizado a restruturação das pastas dos arquivos para melhor organização

--- frontEnd/src/app/app-routing.module.ts
+++ frontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DisciplineRegistrationComponent } from './pages/discipline-registration/discipline-registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'register',
+    component:DisciplineRegistrationComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontEnd/src/app/app.module.ts
+++ frontEnd/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
+import { FooterComponent } from './components/footer/footer.component';
 import {NgOptimizedImage} from "@angular/common";
-import { HeaderComponent } from './header/header.component';
+import { HeaderComponent } from './components/header/header.component';
+import { DisciplineRegistrationComponent } from './pages/discipline-registration/discipline-registration.component';
+import { TitleComponent } from './components/title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    DisciplineRegistrationComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
