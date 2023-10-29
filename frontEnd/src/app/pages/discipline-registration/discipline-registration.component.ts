07c468d09e12934b011881dbd524628934dc907a
Maurício Simões <mauricio.nh@gmail.com>
2023-10-29 14:25:57 -0300

    Validação discipline

--- frontEnd/src/app/pages/discipline-registration/discipline-registration.component.ts
+++ frontEnd/src/app/pages/discipline-registration/discipline-registration.component.ts
@@ -6,12 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./discipline-registration.component.css']
 })
 export class DisciplineRegistrationComponent {
+
+  validarCursoSelecionado() {
+    return this.formData.curso !== 'Selecione um curso';
+  }
+
+  converterParaMaiusculas() {
+    this.formData.codigo = this.formData.codigo.toUpperCase();
+    this.formData.disciplina = this.formData.disciplina.toUpperCase();
+  }
+
   formData: any = {
+    codigo: '', // Inicialize o código como uma string vazia
     curso: 'Selecione um curso'
   };
 
   public listCursos: Array<{ curso: string }> = [
-    { curso:'Selecione um curso' },
+    { curso: 'Selecione um curso' },
     { curso: 'Licenciatura em Letras Português e Espanhol' },
     { curso: 'Tecnologia em Análise e Desenvolvimento de Sistemas' },
     { curso: 'Tecnologia em Eletrônica Industrial' },
