3021c979bcc3ed7d47c32d7a8c1f0220c8a1ba66
GuilhermeSP23 <guiseuper2@gmail.com>
2023-11-01 00:05:10 -0300

    Criação de modelos

---
+++ frontEnd/src/app/model/Ppc.ts
@@ -0,0 +1,10 @@
+import { Curso } from "./Curso";
+import { Disciplina } from "./Disciplina";
+
+export interface Ppc {
+  id: string;
+  nomePPC: string;
+  ano: number;
+  cursos: Curso[];
+  disciplina?: Disciplina[];
+}
