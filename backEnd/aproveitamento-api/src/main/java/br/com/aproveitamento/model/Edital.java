6939736876d89afe427a7cd8c8df468aae76f58a
Jackson <balestjackson@gmail.com>
2023-11-08 01:28:32 -0300

    Inicial listar editais

--- backEnd/aproveitamento-api/src/main/java/br/com/aproveitamento/model/Edital.java
+++ backEnd/aproveitamento-api/src/main/java/br/com/aproveitamento/model/Edital.java
@@ -4,18 +4,15 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Data;
 
@@ -40,6 +37,7 @@ public class Edital {
 	@OneToMany(mappedBy = "edital", cascade = CascadeType.ALL,  orphanRemoval = true)
 	private List<Etapa> etapas = new ArrayList<>();
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "edital", cascade = CascadeType.ALL,  orphanRemoval = true)
 	private List<Requisicao> requisicoes = new ArrayList<>();
 	
--- frontEnd/src/app/pages/announcement/announcement-list/announcement-list.component.ts
+++ frontEnd/src/app/pages/announcement/announcement-list/announcement-list.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Edital } from 'src/app/model/Edital';
+import { EditalService } from 'src/app/services/edital.service';
 
 @Component({
   selector: 'app-announcement-list',
   templateUrl: './announcement-list.component.html',
   styleUrls: ['./announcement-list.component.css']
 })
-export class AnnouncementListComponent {
+export class AnnouncementListComponent implements OnInit {
+
+  public editalList: Edital[] = [];
+
+  constructor(private route: ActivatedRoute,
+              private editalService: EditalService){
+  }
+
+  ngOnInit(): void {
+    this.getAnnouncement();
+  }
+
+  getAnnouncement(){
+    this.editalService.list().subscribe(
+      (_edital: Edital[]) => {
+        this.editalList = _edital;
+      },
+      error => console.log(error)
+    )
+
+  }
+
+  toDate(dateString: string): string {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
 
 }
