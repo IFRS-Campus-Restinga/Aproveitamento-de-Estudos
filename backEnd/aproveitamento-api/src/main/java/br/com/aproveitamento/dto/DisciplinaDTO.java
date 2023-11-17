36642524fac55def532f9a32bc20f45da378b067
GuilhermeSP23 <guiseuper2@gmail.com>
2023-11-17 18:44:16 -0300

    DTO da Disciplina Criado

---
+++ backEnd/aproveitamento-api/src/main/java/br/com/aproveitamento/dto/DisciplinaDTO.java
@@ -0,0 +1,10 @@
+/*Desenvolvido por Guilherme Selau Pereira */
+package br.com.aproveitamento.dto;
+
+public record DisciplinaDTO(
+        Long id,
+        String nome,
+        String codDisciplina,
+        int cargaHoraria) {
+
+}
--- frontEnd/src/app/pages/discipline/discipline-list/discipline-list.component.ts
+++ frontEnd/src/app/pages/discipline/discipline-list/discipline-list.component.ts
@@ -1,3 +1,4 @@
+/*Desenvolvido por Guilherme Selau Pereira*/
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./discipline-list.component.css']
 })
 export class DisciplineListComponent {
+  /*public disciplinaList: Disciplina[] = [];
+  public disciplinaDelete!: Disciplina;
+  @Output() edit = new EventEmitter(false);
+  @Output() delete = new EventEmitter(false);
+  public isConfirmationVisible: boolean | undefined;
+  public confirmationMessage = 'Tem certeza que deseja excluir está Disciplina?';
 
+  constructor(private route: ActivatedRoute,
+              private disciplinaService: DisciplinaService,
+              private router: Router){
+  }
+
+  ngOnInit(): void {
+    this.getAnnouncement();
+  }
+
+  getAnnouncement(){
+    this.disciplinaService.list().subscribe(
+      (_disciplina: Disciplina[]) => {
+        this.disciplinaList = _disciplina;
+      },
+      error => console.log(error)
+    )
+
+  }
+
+  toDate(dateString: string): string {
+    const date = new Date(dateString);
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+
+
+  onEdit(disciplina: Disciplina) {
+    this.router.navigate(['edit', disciplina.id], {relativeTo: this.route});
+  }
+
+  showConfirmationDialog(disciplina: Disciplina) {
+    this.isConfirmationVisible = true;
+    this.disciplinaDelete = disciplina;
+  }
+
+  handleConfirmation(confirmed: boolean) {
+    if (confirmed) {
+      this.disciplinaService.remove(this.disciplinaDelete?.id).subscribe(
+        result => alert("Disciplina deletada com sucesso"),
+        error => alert("Erro ao deletar disciplina")
+      );
+    }
+    this.isConfirmationVisible = false;
+  }*/
 }
