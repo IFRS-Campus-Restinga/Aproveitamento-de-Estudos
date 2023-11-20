3df3d58e273769b9994de893e43a54bb3ca7607a
GuilhermeSP23 <guiseuper2@gmail.com>
2023-11-20 19:53:30 -0300

    Modificações no DisciplinaService do FRONT

---
+++ frontEnd/src/app/services/disciplina.service.ts
@@ -0,0 +1,47 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { first } from 'rxjs';
+import { Disciplina } from '../model/Disciplina';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DisciplinaService {
+
+  private readonly API = 'api/edital';
+
+  constructor(private httpClient: HttpClient) { }
+
+  list() {
+    return this.httpClient.get<Disciplina[]>(this.API)
+      .pipe(
+        first()
+      );
+  }
+
+  loadById(id: string) {
+    return this.httpClient.get<Disciplina>(`${this.API}/${id}`);
+  }
+
+  save(record: Partial<Disciplina>) {
+    console.log(record);
+    if (record.id) {
+      console.log('update');
+      return this.update(record);
+    }
+    console.log('create');
+    return this.create(record);
+  }
+
+  private create(record: Partial<Disciplina>) {
+    return this.httpClient.post<Disciplina>(this.API, record).pipe(first());
+  }
+
+  private update(record: Partial<Disciplina>) {
+    return this.httpClient.put<Disciplina>(`${this.API}`, record).pipe(first());
+  }
+
+  remove(id: string) {
+    return this.httpClient.delete(`${this.API}/${id}`).pipe(first());
+  }
+}
