a44cace958e1989ae0b7404562b16a1b418f5dd3
GuilhermeSP23 <guiseuper2@gmail.com>
2023-12-03 15:45:45 -0300

    FRONT: Validação da Carga Horaria da Disciplina

--- frontEnd/src/app/pages/discipline/discipline-registration/discipline-registration.component.ts
+++ frontEnd/src/app/pages/discipline/discipline-registration/discipline-registration.component.ts
@@ -55,7 +55,7 @@ export class DisciplineRegistrationComponent implements OnInit {
       disciplina: [disciplina.nome, [Validators.required, 
         Validators.pattern(/^(?!.*[.]{2})(?!.*[,]{2})(?!.*[\s]{2})[a-zA-ZÀ-ÖØ-öø-ÿ0-9\s]*(?:[.,]\s?[a-zA-ZÀ-ÖØ-öø-ÿ0-9\s]*)*$/), 
         Validators.minLength(10),Validators.maxLength(120)]],
-      cargaHoraria: [disciplina.cargaHoraria, [Validators.required, Validators.min(10), Validators.max(500)]],
+      cargaHoraria: [disciplina.cargaHoraria, [Validators.required, Validators.min(10), Validators.max(88)]],
     });
      
   }
